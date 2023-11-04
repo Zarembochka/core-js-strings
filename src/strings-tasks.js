3d4560316a142b7cf32d3646968c4be61d434682
Luba Hadunova <luba.godunova2018@gmail.com>
2023-11-04 21:34:04 +0100

    feat: add fourth part of functions

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -242,8 +242,10 @@ function endsWith(str, substr) {
  *   formatTime(0, 45) => "00:45"
  *   formatTime(0, 0) => "00:00"
  */
-function formatTime(/* minutes, seconds */) {
-  throw new Error('Not implemented');
+function formatTime(minutes, seconds) {
+  const stringMinutes = minutes.toString().padStart(2, '0');
+  const stringSeconds = seconds.toString().padStart(2, '0');
+  return stringMinutes.concat(':', stringSeconds);
 }
 
 /**
@@ -256,8 +258,8 @@ function formatTime(/* minutes, seconds */) {
  *   reverseString('abcdef') => 'fedcba'
  *   reverseString('12345') => '54321'
  */
-function reverseString(/* str */) {
-  throw new Error('Not implemented');
+function reverseString(str) {
+  return str.split('').reverse().join('');
 }
 
 /**
@@ -271,8 +273,8 @@ function reverseString(/* str */) {
  *   orderAlphabetically('textbook') => 'bekoottx'
  *   orderAlphabetically('abc123xyz') => '123abcxyz'
  */
-function orderAlphabetically(/* str */) {
-  throw new Error('Not implemented');
+function orderAlphabetically(str) {
+  return str.split('').sort().join('');
 }
 
 /**
@@ -287,8 +289,8 @@ function orderAlphabetically(/* str */) {
  *   containsSubstring('JavaScript is Fun', 'Python') => false
  *   containsSubstring('12345', '34') => true
  */
-function containsSubstring(/* str, substring */) {
-  throw new Error('Not implemented');
+function containsSubstring(str, substring) {
+  return str.includes(substring);
 }
 
 /**
@@ -305,8 +307,9 @@ function containsSubstring(/* str, substring */) {
  *   countVowels('aEiOu') => 5
  *   countVowels('XYZ') => 1
  */
-function countVowels(/* str */) {
-  throw new Error('Not implemented');
+function countVowels(str) {
+  const vowels = ['a', 'e', 'i', 'o', 'u', 'y', 'A', 'E', 'I', 'O', 'U', 'Y'];
+  return str.split('').filter((a) => vowels.includes(a)).length;
 }
 
 /**
@@ -322,8 +325,15 @@ function countVowels(/* str */) {
  *   isPalindrome('apple') => false
  *   isPalindrome('No lemon, no melon') => true
  */
-function isPalindrome(/* str */) {
-  throw new Error('Not implemented');
+function isPalindrome(str) {
+  const lastSymbol = str.at(-1);
+  const reg = /,\s|\s/;
+  let startsStr = str.split(reg).join('');
+  if (lastSymbol === '!' || lastSymbol === '?') {
+    startsStr = startsStr.slice(0, -1);
+  }
+  const reverseStr = startsStr.split('').reverse().join('');
+  return startsStr.toUpperCase() === reverseStr.toUpperCase();
 }
 
 /**
@@ -338,8 +348,11 @@ function isPalindrome(/* str */) {
  *   findLongestWord('A long and winding road') => 'winding'
  *   findLongestWord('No words here') => 'words'
  */
-function findLongestWord(/* sentence */) {
-  throw new Error('Not implemented');
+function findLongestWord(sentence) {
+  return sentence
+    .split(' ')
+    .sort((a, b) => b.length - a.length)
+    .at(0);
 }
 
 /**
